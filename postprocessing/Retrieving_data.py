d0372138eac024f0e6613d66cdba71bad4fa1fc2
JelleLagerweij <v.j.lagerweij@student.tudelft.nl>
2023-02-22 12:27:51 +0100

    postprocessing done

--- postprocessing/Retrieving_data.py
+++ postprocessing/Retrieving_data.py
@@ -16,30 +16,30 @@ time = [0]*len(t)
 for i in range(len(t)):
     time[i] = str(t[i]) + 'ns'
 
-# for i in range(len(t)):
-i = 18
-folder = loc+time[i]  # Path to the main folder
+for i in range(len(t)):
+    # i = 16
+    folder = loc+time[i]  # Path to the main folder
 
-f_runs = ['1', '3', '4']  # All internal runs
-groups = ['wat', 'Li', 'S']
+    f_runs = ['1', '2', '3', '4']  # All internal runs
+    groups = ['wat', 'Na', 'B']
 
-# Load the class
-mixture = octp.PP_OCTP(folder, f_runs, groups, plotting=True)
+    # Load the class
+    mixture = octp.PP_OCTP(folder, f_runs, groups, plotting=False)
 
-# Change the file names
-mixture.filenames(Diff_Onsag='onsagercoefficient.dat',
-                  T_conduc='thermconductivity.dat')
+    # Change the file names
+    mixture.filenames(Diff_Onsag='onsagercoefficient.dat',
+                      T_conduc='thermconductivity.dat')
 
-mixture.changefit(Minc=12, Mmax=15)
-mixture.pressure(mov_ave=100)
-mixture.tot_energy(mov_ave=100)
-mixture.pot_energy(mov_ave=100)
-mixture.density()
-mixture.molarity('S')
-mixture.molality('S', 'wat', 18.01528)
-mixture.viscosity()
-# mixture.thermal_conductivity()
-mixture.self_diffusivity(YH_correction=True, box_size_check=True)
-mixture.onsager_coeff(box_size_check=True)
+    mixture.changefit(Minc=8, Mmax=15)
+    mixture.pressure(mov_ave=100)
+    mixture.tot_energy(mov_ave=100)
+    mixture.pot_energy(mov_ave=100)
+    mixture.density()
+    mixture.molarity('B')
+    mixture.molality('B', 'wat', 18.01528)
+    mixture.viscosity()
+    # mixture.thermal_conductivity()
+    mixture.self_diffusivity(YH_correction=True)
+    mixture.onsager_coeff()
 
-mixture.store(location='', name=time[i] + '.csv')
+    mixture.store(location='', name=time[i] + '.csv')
